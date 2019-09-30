8872e9bc7c510deb5a6f6623681921ea805a5dae
Felipe Araujo <devaraujofelipe@gmail.com>
2019-09-30 15:56:29 -0300

    Use localStorage to save repos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -14,6 +14,24 @@ export default class Main extends Component {
     };
   }
 
+  // Load data from localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Save data on localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
